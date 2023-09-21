617d1fcafe5213c0f78a64af838c62d08d48a3fe
George Merida <gmerida92@gmail.com>
2023-09-20 17:12:21 -0700

    Phase 0 Complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,22 @@
 // backend/routes/index.js
-// Creating a test route
 
 const express = require('express');
+const { restart } = require('nodemon');
 const router = express.Router();
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    })
+})
+
+// Creating a test route
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 module.exports = router;
